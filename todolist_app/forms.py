571f312a81b4abd8d5b5d666957d5dd3f60f445c
mohamedbenaantar <mohamedbenantar2000@gmail.com>
2024-01-12 10:52:00 +0100

    submit a task to the database

---
+++ todolist_app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from todolist_app.models import TaskList
+
+## which database I'm connecting to 
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = TaskList
+        fields = ['task', 'done'] ## mention the fields that I'm going to edit
+        
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import TaskList
+from .forms import TaskForm
 # Create your views here.
 
 def todolist(request):
-    all_tasks = TaskList.objects.all()
-    return render(request, 'todolist.html',{'all_tasks':all_tasks})
+    if request.method == 'POST':
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return redirect('todolist') 
+    else: ## Get request
+        all_tasks = TaskList.objects.all()
+        return render(request, 'todolist.html',{'all_tasks':all_tasks})
 
 def contactus(request):
     content = {
